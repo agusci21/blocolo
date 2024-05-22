6813872837044b43ead946a30393c8c011db1b58
Franco Agustín Cola Ibáñez <agustincola54@gmail.com>
2024-05-22 15:24:33 -0300

    finish

---
+++ src/main/java/com/llbafaci/blocolo/dtos/StudentTaskDto.java
@@ -0,0 +1,45 @@
+package com.llbafaci.blocolo.dtos;
+
+import com.llbafaci.blocolo.helpers.DatabaseConnection;
+
+public class StudentTaskDto {
+
+    private int taskId;
+    private int studentId;
+
+    public StudentTaskDto(int taskId, int studentId) {
+        this.taskId = taskId;
+        this.studentId = studentId;
+    }
+
+    public static void createTableIfNotExist() {
+        String query = "CREATE TABLE IF NOT EXISTS studentsTasks (" +
+                "studentId INTEGER NOT NULL," +
+                "taskId INTEGER NOT NULL," +
+                "PRIMARY KEY (studentId, taskId))";
+
+        try {
+            DatabaseConnection connection = DatabaseConnection.getConnection();
+            connection.execute(query);
+            System.out.println("Tabla 'StudentTask' creada exitosamente.");
+        } catch (Exception e) {
+            System.out.println("Error al crear la tabla 'StudentTask': " + e.getMessage());
+        }
+    }
+
+    public String toTable() {
+        String query = "INSERT INTO studentsTasks (studentId, taskId) VALUES (" +
+                studentId + ", '" +
+                taskId + "', ');";
+        return query;
+    }
+
+    public int getTaskId() {
+        return taskId;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+}
--- src/main/java/com/llbafaci/blocolo/helpers/TableCreatorHelper.java
+++ src/main/java/com/llbafaci/blocolo/helpers/TableCreatorHelper.java
@@ -1,11 +1,13 @@
 package com.llbafaci.blocolo.helpers;
 
 import com.llbafaci.blocolo.dtos.StudentDto;
+import com.llbafaci.blocolo.dtos.StudentTaskDto;
 import com.llbafaci.blocolo.dtos.TaskDto;
 
 public class TableCreatorHelper {
     public static void createTablesIfNotExist() {
         StudentDto.createTableIfNotExist();
         TaskDto.createTableIfNotExist();
+        StudentTaskDto.createTableIfNotExist();
     }
 }
